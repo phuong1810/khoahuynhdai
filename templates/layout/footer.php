a229a0a916c9c265c9c8116a85ac733c9de9cb5d
duybao1018 <baobaophph1@gmail.com>
2025-08-09 15:45:31 +0700

    update footer bao

--- templates/layout/footer.php
+++ templates/layout/footer.php
@@ -2,45 +2,85 @@
     <div class="container">
         <div class="row justify-content-between">
             <div class="col-12 col-sm-12 col-md-6 cont-foot1">
-                <div class="title-footer"><?=$footer['name'.$lang]?></div>
-                <a class="logo-footer" href="">
-                    <?= $func->getImage(['sizes' => '', 'upload' => UPLOAD_NEWS_L, 'image' => $footer['photo'], 'alt' => $setting['name' . $lang]]) ?>
-                </a>
-                <div class="cont-footer">
-                    <?= $func->decodeHtmlChars($footer['content' . $lang]) ?>
-                </div>
+               <div>
+                    <div class="title-footer">CÔNG TY TNHH TM & DV KC HOME</div>
+                    <div class="content-foot1">
+                        MST: 0318018538 <br>
+                        Hotline: 0944 405 482 (24/7) <br>
+                        Head Office: 482 Trường Chinh, P. Tân Hưng Thuận, Q.12, HCM
+                    </div>
+                    <div class="content-foot2">
+                        CHI NHÁNH TÂN PHÚ (Hồ Chí Minh) <br>
+                        Địa chỉ: 378/57 Thoại Ngọc Hầu, P. Phú Thạnh, Q. Tân Phú
+                    </div>
+                    <div>
+                        CHI NHÁNH HAI BÀ TRƯNG (Hà Nội) <br>
+                        Địa chỉ: 20 Trương Định, P. Trương Định, Q. Hai Bà Trưng
+                    </div>
+               </div>
             </div>
-            <div class="col-12 col-sm-12 col-md-6 cont-foot2">
-                <div class="title-footer">TƯ VẤN ĐẶT HÀNG</div>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+            <div class="col-12 col-sm-5 col-md-2 cont-foot2">
+                <div class="title-footer">TỔNG ĐÀI HỖ TRỢ </div>
                 <div class="info-footer">
-                    <div class="d-flex align-items-center justify-content-between info-footer--v2">
-                        <div class="phone-footer"><?=$optsetting['phone']?></div>
-                        <div class="email-footer"><?=$optsetting['email2']?><br><?=$optsetting['email3']?></div>
-                        <div class="gopy-footer">Góp ý về dịch vụ<br><a href="tel:<?=preg_replace('/[^0-9]/','',$optsetting['hotline']);?>"><?=$optsetting['hotline']?></a></div>
+                    <div class="d-flex align-items-center  info-footer--v2">
+                        <ul class="list">
+                            <li class="icon">
+                                <i class="fa-solid fa-phone"></i>
+                                Tư vấn trực tiếp
+                                <br>
+                                <span>0944405482</span>
+                            </li>
+                            <li class="icon">
+                                <i class="fa-solid fa-mobile-screen"></i>
+                                Khiếu nại & Bảo hành
+                                <br>
+                                <span>0944405482</span>
+                            </li>
+                            <li>
+                                <em>Phục vụ 24/7(cả ngày lễ và chủ nhật)</em>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <div class="dslienhe">
-                    <?php foreach($dslienhe as $v) {?>
-                        <div class="item-ds">
-                            <a href="tel:<?=preg_replace('/[^0-9]/','',$v['desc'.$lang]);?>">
-                                <img onerror="this.src='<?=THUMBS?>/39x39x1/assets/images/noimage.png';" src="<?=THUMBS?>/39x39x1/<?=UPLOAD_NEWS_L.$v['photo']?>" alt="<?=$v['name'.$lang]?>">
-                                <div class="title">
-                                    <p class="titl"><?=$v['name'.$lang]?></p>
-                                    <p><?=$v['desc'.$lang]?></p>
-                                </div>
-                            </a>
-                        </div>
-                    <?php } ?>
-                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 cont-foot4">
+               <div class="col-inner">
+                    <p>
+                        <strong>SẢN PHẨM</strong>
+                    </p>
+                    <p class="sp-color">
+                        <strong>
+                            <a href="https://hyundaismartlock.vn/danh-muc-san-pham/khoa-cua-nhom/">Khóa cửa nhôm</a>
+                        </strong>
+                        <br>
+                        <strong>
+                            <a href="https://hyundaismartlock.vn/danh-muc-san-pham/khoa-cua-go/">Khóa cửa gỗ</a>
+                        </strong>
+                        <br>
+                        <strong>
+                            <a href="https://hyundaismartlock.vn/danh-muc-san-pham/khoa-cua-cong/">Khóa cửa cổng</a>
+                        </strong>
+                        <br>
+                        <a href="https://hyundaismartlock.vn/danh-muc-san-pham/khoa-khach-san/">
+                            <strong>Khóa khách sạn</strong>
+                        </a>
+                        <br>
+                        <a href="https://hyundaismartlock.vn/danh-muc-san-pham/khoa-face-id/">
+                            <strong>Khóa Face ID</strong>
+                        </a>
+                        <br>
+                        <a href="https://hyundaismartlock.vn/danh-muc-san-pham/khoa-dai-sanh/">
+                            <strong>Khóa biệt thự, đại sảnh</strong>
+                        </a>
+                    </p>
+		        </div>
+
+                
+
 
             </div>
             <div class="col-12 col-sm-12 col-md-6 cont-foot3">
-                <div class="title-footer">HƯỚNG DẪN VÀ THÔNG TIN</div>
-                <ul class="mb24">
-                    <?php foreach($huongdan as $k=>$v){if($k<6){?>
-                        <li><a href="<?=$v[$sluglang]?>" title="<?=$v['name'.$lang]?>"><span>- <?=$v['name'.$lang]?></span></a></li>
-                    <?php } } ?>
-                </ul>
                 <div class="title-footer">CHÍNH SÁCH</div>
                 <ul class="mb24">
                     <?php foreach($chinhsach as $k=>$v){if($k<6){?>
@@ -50,24 +90,9 @@
                 <div class="title-footer">HỢP TÁC & TÀI TRỢ</div>
                 <div class="cont-hoptac"> <?= $func->decodeHtmlChars($optsetting['hoptac']) ?></div>
             </div>
-            <div class="col-12 col-sm-12 col-md-6 cont-foot4">
-                <div class="row">
-                    <div class="col-12 col-sm-12 col-md-12 col-lg-12">
-                        <div class="title-footer">PHƯƠNG THỨC THANH TOÁN</div>
-                        <img class="mb24" src="assets/images/phuongthuc.png" alt="Thanh toán được chấp nhận">
-                    </div>
-
-                </div>
-
-
-            </div>
+            
         </div>
-        <div class="wrap-address">
-            <div class="row justify-content-between">
-                <div class="col-12 col-sm-6 col-12--v2"><div class="item"><span>Văn Phòng: </span><?=$optsetting['vanphong']?></div></div>
-                <div class="col-12 col-sm-6"><div class="item"><span>Xưởng sản xuất: </span><?=$optsetting['xuongsanxuat']?></div></div>
-            </div>
-        </div>
+       
 
         <div class="copyright">
             <?=$optsetting['copyright']?>
